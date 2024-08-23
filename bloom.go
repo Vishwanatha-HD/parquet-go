f71316d27e918bfceb11749bbaf6032ba27c7d45
Vishwanatha HD <Vishwanatha.HD@ibm.com>
2024-08-23 06:35:08 +0000

    Fix TestSplitBlockFilter/INT96 testcase failure

--- bloom.go
+++ bloom.go
@@ -2,7 +2,10 @@ package parquet
 
 import (
 	"io"
+	"fmt"
+	"encoding/binary"
 
+	"golang.org/x/sys/cpu"
 	"github.com/parquet-go/parquet-go/bloom"
 	"github.com/parquet-go/parquet-go/bloom/xxhash"
 	"github.com/parquet-go/parquet-go/deprecated"
@@ -172,7 +175,22 @@ func (splitBlockEncoding) EncodeInt64(dst []byte, src []int64) ([]byte, error) {
 }
 
 func (e splitBlockEncoding) EncodeInt96(dst []byte, src []deprecated.Int96) ([]byte, error) {
-	splitBlockEncodeFixedLenByteArray(bloom.MakeSplitBlockFilter(dst), deprecated.Int96ToBytes(src), 12)
+        if cpu.IsBigEndian {
+		fmt.Printf("Vishwa>>>> (before) src = %#v \n\n", src)
+
+                srcLen := len(src)
+                buf := make([]byte, srcLen*12)
+                for idx := 0; idx < srcLen; idx++ {
+                        binary.LittleEndian.PutUint32(buf[(idx*12):4+(idx*12)], uint32(src[idx][0]))
+                        binary.LittleEndian.PutUint32(buf[4+(idx*12):8+(idx*12)], uint32(src[idx][1]))
+                        binary.LittleEndian.PutUint32(buf[8+(idx*12):12+(idx*12)], uint32(src[idx][2]))
+                }
+
+		fmt.Printf("Vishwa>>>> (after) buf = %#v, src = %#v \n\n", buf, src)
+	        splitBlockEncodeFixedLenByteArray(bloom.MakeSplitBlockFilter(dst), buf, 12)
+	} else {
+		splitBlockEncodeFixedLenByteArray(bloom.MakeSplitBlockFilter(dst), deprecated.Int96ToBytes(src), 12)
+	}
 	return dst, nil
 }
 
